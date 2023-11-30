00f8e7f377591bbd5617bf93421361a15ceb9ea0
diego <dinsaurralde@yahoo.com.ar>
2023-11-30 18:03:43 -0300

    Correccion Back

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 /*import path from "path";*/
-import multer from "multer";
 
 /*import { config } from "./settings/config.js";
 import { startConnection } from "./settings/database.js";*/
@@ -17,20 +16,6 @@ const app = express();
 dotenv.config();
 app.use(express.json());
 /*app.use("/images", express.static(path.join(__dirname, "/images")));*/
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, req.body.name);
-  },
-});
-
-const upload = multer({ storage: storage });
-app.post("/api/upload", upload.single("file"), (req, res) => {
-  res.status(200).json("File has been uploaded");
-});
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
